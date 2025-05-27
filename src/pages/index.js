0fe4a280f4d588a1ad50b30b234bd234ef378a0a
Dhyan A <dhyan.colours@gmail.com>
2025-05-27 22:41:20 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
         </div>
         <div className='w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center'>
   
-          <AnimatedText text="  Blending Creativity            and Code to Shape            a Better Tomorrow." className='!text-6xl !text-left xl:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl'/>
+          <AnimatedText text="  Blending Creativity<br/>and Code to Shape<br/>a Better Tomorrow." className='!text-6xl !text-left xl:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl'/>
           <p className='my-4 text-base font-medium md:text-sm sm:text-xs justify'>Hi, I’m Dhyan A, a tech enthusiast passionate about blending innovation with impact. With expertise in programming, AI/ML, and data analytics, I thrive on solving real-world problems through smart technology and creative design. Beyond code, I’m a lifelong learner and a leader who loves turning ideas into meaningful solutions. Let’s innovate together!</p>
 <div className='flex items-center self-start mt-2 lg:self-center'>
   <Link href="/Dhyan A Resume.pdf" target={"_blank"} className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base' download={true}>Resume <LinkArrow className={"w-6 ml-1"}/></Link>
